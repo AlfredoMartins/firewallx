1bf0e3dd4bfa340e24515fda58dee50e039862d1
Alfredo Martins <34483729+CodeTyperPro@users.noreply.github.com>
2024-12-25 17:40:37 +0000

    feat(custom requests, error handling): wsgi called, error handling created and custom requests added

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -2,11 +2,13 @@ from flask import Flask
 from flask_cors import CORS
 from config.config import Config
 from dotenv import load_dotenv
+from middlewares.custom_middleware import CustomMiddleware
 
 # Load environment variables
 load_dotenv()
 
 app = Flask(__name__)
+app.wsgi_app = CustomMiddleware(app.wsgi_app)
 app.config['SECRET_KEY'] = 'secret!'
 
 CORS(app, origins=["http://localhost:5173", "http://192.168.1.77:5173"], supports_credentials=True)
--- backend/src/routes/api.py
+++ backend/src/routes/api.py
@@ -13,3 +13,27 @@ def delete_blocked_ip(id):
     print("IP: ", id)
     firewall.allow(id)
     return 'Blocked IP Logs...'
+
+# Custom requests
+@api.before_request
+def before_request():
+    pass
+
+@api.after_request
+def after_request():
+    pass
+
+@api.teardown_request
+def teardown_request():
+    pass
+
+
+# Error Handling
+@api.errorhandler(404)
+def page_not_found(e):
+    return "Page not found :(", 404
+
+@api.errorhandler(500)
+def internal_server_error(e):
+    return "Internal server error", 500
+
